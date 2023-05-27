5678b3f996548d997bcf7a859ff50e5098d183e1
harrybarcia12 <barcia.harry@gmail.com>
2023-05-27 16:31:55 -0700

    adding markers

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,46 @@
 import './App.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 
 function App() {
+  const [markers, setMarkers] = useState<any[]>([]);
 
-  async function fetchData() {
-  try {
+  useEffect(() => {
     axios("http://localhost:3002/api/users").then((response) => {
+
       console.log(response.data);
     });
-  } catch (error) {
-    // Handle any network or request errors
-    console.error('Error:', error.message);
-  }
-}
+  }, []);
 
-fetchData();
 
-async function loadMarkers() {
-  try {
+
+  useEffect(() => {
     axios("http://localhost:3002/api/markers").then((response) => {
+      setMarkers(response.data);
       console.log(response.data);
+    
     });
-  } catch (error) {
-    // Handle any network or request errors
-    console.error('Error:', error.message);
+
   }
-}
-loadMarkers();
+  , []);
+
 
   return(
     <div style={{ width: '100%', height: '100vh' }}>
-        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ width: '100%', height: '100%' }} >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      <MapContainer center={[45, -123]} zoom={5} style={{ width: '100%', height: '100%' }} >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {markers.map((marker) => (
+          <Marker position={[marker.lat, marker.lng]}>
+            <Popup>
+              {marker.data.markerType}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
       
