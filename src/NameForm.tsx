f5ca4d51120a0b376eece2dcdcc2caebb0d8ed5c
harrybarcia12 <barcia.harry@gmail.com>
2023-07-27 11:45:01 -0700

    adding the edit mode on the popup

--- src/NameForm.tsx
+++ src/NameForm.tsx
@@ -8,6 +8,8 @@ function NameForm(props: any) {
   const [markerCustomSubType, setMarkerCustomSubType] = useState(data.markerCustomSubType?data.markerCustomSubType:"test")
   const [markerType, setMarkerType] = useState(data.markerType)
   const [rating, setRating] = useState(data.rating)
+  const [editMode, setEditMode] = useState(false);
+
   
   const handleSubmit = async (e: any) => {
     const updateMarker = async () => {
@@ -29,51 +31,62 @@ function NameForm(props: any) {
     updateMarker()
     e.preventDefault();
     props.onClosePopup();
-  }
+  } 
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <div style={{
-        "display": "flex", "flexDirection": "column", "padding": "1em"
-      }}>
-        <label>
-          Type:
-          <input
-            type="text"
-            defaultValue={data.markerType}
-            onChange={(e) => setMarkerType(e.target.value)}
-            style={{ "display": "flex" }}
-          />
-        </label>
-        <label>
-          markerCustomSubType:
-          <input
-            type="text"
-            defaultValue={data.markerCustomSubType}
-            onChange={(e) => setMarkerCustomSubType(e.target.value)}
-            style={{ "display": "flex" }}
-          />
-        </label>
-        <label>
-          rating:
-          <input
-            type="text"
-            defaultValue={data.rating}
-            onChange={(e) => setRating(e.target.value)}
-            style={{ "display": "flex" }}
-          />
-        </label>
-      </div>
-      <button type="submit"
-        className="closeOnSubmit"
-      >Submit</button>
-    </form>
-      <button type="submit"
-        className="closeOnSubmit"
-        onClick={props.saveMarker}
-      >Submit the new Marker</button>
-      </>
+      {editMode ? (
+        <form onSubmit={handleSubmit}>
+          <div style={{ display: 'flex', flexDirection: 'column', padding: '1em' }}>
+            <label>
+              Type:
+              <input
+                type="text"
+                value={markerType}
+                onChange={(e) => setMarkerType(e.target.value)}
+                style={{ display: 'flex' }}
+              />
+            </label>
+            <label>
+              markerCustomSubType:
+              <input
+                type="text"
+                value={markerCustomSubType}
+                onChange={(e) => setMarkerCustomSubType(e.target.value)}
+                style={{ display: 'flex' }}
+              />
+            </label>
+            <label>
+              rating:
+              <input
+                type="text"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                style={{ display: 'flex' }}
+              />
+            </label>
+          </div>
+          <button type="submit" className="closeOnSubmit">
+            Submit
+          </button>
+        </form>
+      ) : (
+        <div>
+          <div>Type: {markerType}</div>
+          <div>markerCustomSubType: {markerCustomSubType}</div>
+          <div>rating: {rating}</div>
+          <button type="button" onClick={handleEditClick}>
+            Edit
+          </button>
+        </div>
+      )}
+      <button type="submit" className="closeOnSubmit" onClick={props.saveMarker}>
+        Submit the new Marker
+      </button>
+    </>
   );
 }
 
